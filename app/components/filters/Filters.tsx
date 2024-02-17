cb0d6694f47d609f01ff760510efd083b7210248
Abraham <abrahamnhier97@gmail.com>
2024-02-17 16:48:28 +0300

    added styling to filters

--- app/components/filters/Filters.tsx
+++ app/components/filters/Filters.tsx
@@ -1,7 +1,13 @@
 export default function Filters() {
     return (
-        <div>
-            
+        <div className="grid-cols-3">
+          <div className="text-center text-white mb-2 text-lg">
+            Filter
+            </div>     
+            <div className="text-center text-white cursor-pointer">Clear filters</div>  
+            <div>
+                
+                </div>       
         </div>
     )
 }
