a36b253e5d042c624ec3a91354e5ad0bf07691f4
Abraham <abrahamnhier97@gmail.com>
2024-02-17 21:18:15 +0300

    created the location page to display locations

---
+++ app/pages/Location.tsx
@@ -0,0 +1,10 @@
+import Location from "../components/Location";
+
+export default function LocationPage() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold mb-4">Locations</h1>
+            <Location />
+        </div>
+    )
+}
