4ded0bd98504988d28f1b99d033775ff3da3d593
Abraham <abrahamnhier97@gmail.com>
2024-02-18 11:41:03 +0300

    typescript support

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import GetData from "./components/Characters";
 import { Character } from './types/Character';
 
-interface ChracterProps {
-  character: Character
+interface CharacterProps {
+  character: Character;
 }
 
-
-export default function Home<CharacterProps>({character} : ChracterProps) {
+const Home: React.FC<CharacterProps> = ({ character }: CharacterProps) => {
   return (
     <main>
-   <GetData />
-   
-     </main>
+      <GetData />
+    </main>
   );
-}
+};
+
+export default Home;
