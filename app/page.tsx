0f5858547d4600652b09bf32b1782f8a0e096a3c
Abraham <abrahamnhier97@gmail.com>
2024-02-18 09:21:59 +0300

    typescript error

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import GetData from "./components/Characters";
 import LocationPage from './pages/Location';
+import { Character } from './types/Character';
+
+
+interface Props {
+  character: Character;
+}
 
 
 
-
-export default function Home() {
+export default function Home({character}: Props) {
   return (
     <main>
    <GetData />
