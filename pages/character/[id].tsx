7a996ae0d7616d148f2cfff9635d97d66b6178f5
Abraham <abrahamnhier97@gmail.com>
2024-02-18 15:14:57 +0300

    setup add notes

--- pages/character/[id].tsx
+++ pages/character/[id].tsx
@@ -12,6 +12,8 @@ interface Props {
 
 export default function CharacterDetails({ character }: Props ) {
    const [details, setDetails] = useState<Character>(character)
+   // notes
+   const [notes, setNotes] = useState<string>("")
     const params = useParams() 
     const router = useRouter()
       
@@ -33,14 +35,26 @@ export default function CharacterDetails({ character }: Props ) {
         useEffect(() => {
        fetchDetails()
       },[params?.id])
+      //back to home page
        const ExitHandler = () => {
             router.push('/')
        }
+       // save notes
+       const saveNotesHandler = () => {
+        //get notes from local storage
+        const existingNotes = JSON.parse(localStorage.getItem("characterNotes") || "{}");
+        existingNotes[params.id] = notes
+        //save notes to local storage
+        localStorage.setItem("characterNotes", JSON.stringify(existingNotes))
+
+        setNotes("")
+       }
+
 
   return (
     <div style={{background: "black"}}>
-       
-        <div style={{margin: "26px auto 0", padding: "0 26px", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+       <div 
+        style={{margin: "26px auto 0", padding: "0 26px", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
            <img
             className="rounded-lg"
             width={300}
@@ -51,28 +65,78 @@ export default function CharacterDetails({ character }: Props ) {
             style={{ textAlign: "center", margin: "26px auto", color: "white", fontSize: "24px"}}>
              {details?.name}
              </h1>
-
-            <div style={{display: "flex",flexDirection: "column", alignItems: "center", justifyContent: "center", border: "2px solid gray", width: "100%", maxWidth: "300px", marginBottom: "26px", padding: "8px", borderRadius: "8px" }}>
+             
+             
+                 
+            <div
+             style={{display: "flex",flexDirection: "column", alignItems: "center", justifyContent: "center", border: "2px solid gray", width: "100%", maxWidth: "300px", marginBottom: "26px", padding: "8px", borderRadius: "8px" }}>
                 <strong style={{color: "yellow", textAlign: "center",fontSize: "20px"}}>Tipo</strong>
                 <span style={{color: "white", textAlign: "center"}}>{details?.type}</span>
             </div>
-            <div style={{display: "flex",flexDirection: "column", alignItems: "center", justifyContent: "center", border: "2px solid gray", width: "100%", maxWidth: "300px", marginBottom: "26px", padding: "8px", borderRadius: "8px" }}>
+            <div 
+            style={{display: "flex",flexDirection: "column", alignItems: "center", justifyContent: "center", border: "2px solid gray", width: "100%", maxWidth: "300px", marginBottom: "26px", padding: "8px", borderRadius: "8px" }}>
                 <strong style={{color: "yellow", textAlign: "center",fontSize: "20px"}}>Origin</strong>
                 <span style={{color: "white", textAlign: "center"}}>{details?.origin?.name}</span>
             </div>
-            <div style={{display: "flex",flexDirection: "column", alignItems: "center", justifyContent: "center", border: "2px solid gray", width: "100%", maxWidth: "300px", marginBottom: "26px", padding: "8px", borderRadius: "8px" }}>
+            <div
+             style={{display: "flex",flexDirection: "column", alignItems: "center", justifyContent: "center", border: "2px solid gray", width: "100%", maxWidth: "300px", marginBottom: "26px", padding: "8px", borderRadius: "8px" }}>
                 <strong style={{color: "yellow", textAlign: "center",fontSize: "20px"}}>Gender</strong>
                 <span style={{color: "white", textAlign: "center"}}>{details?.gender}</span>
             </div>
-            <div style={{display: "flex",flexDirection: "column", alignItems: "center", justifyContent: "center", border: "2px solid gray", width: "100%", maxWidth: "300px", marginBottom: "26px", padding: "8px", borderRadius: "8px" }}>
+            <div
+             style={{display: "flex",flexDirection: "column", alignItems: "center", justifyContent: "center", border: "2px solid gray", width: "100%", maxWidth: "300px", marginBottom: "26px", padding: "8px", borderRadius: "8px" }}>
                 <strong style={{color: "yellow", textAlign: "center",fontSize: "20px"}}>Status</strong>
                 <span style={{color: "white", textAlign: "center"}}>{details?.status}</span>
             </div>
             <div style={{display: "flex",flexDirection: "column", alignItems: "center", justifyContent: "center", border: "2px solid gray", width: "100%", maxWidth: "300px", marginBottom: "26px", padding: "8px", borderRadius: "8px" }}>
                 <strong style={{color: "yellow", textAlign: "center",fontSize: "20px"}}>Species</strong>
                 <span style={{color: "white", textAlign: "center"}}>{details?.species}</span>
+              
             </div>
+          
         </div>
+        <div 
+        style={{
+            display: "flex",
+            justifyContent: "center",
+        }}
+        >
+            
+        <textarea
+              style={{
+                width: '30%',
+                padding: '8px',
+                margin: '10px 0',
+                borderRadius: '5px',
+                border: '1px solid #ccc',
+                resize: 'none',
+                fontFamily: 'Arial, sans-serif',
+                fontSize: '14px',
+              }}
+          name="note"
+          id="note"
+          placeholder="Enter your note.."
+         col={30}
+         row={10}
+          value={notes}
+          onChange={(e) => setNotes(e.target.value)}
+        ></textarea>
+        <button
+        style={{
+          padding: '10px 10px',
+          backgroundColor: 'orange',
+          color: 'white',
+          border: 'none',
+          borderRadius: '5px',
+          cursor: 'pointer',
+          fontSize: '16px',
+          fontWeight: 'bold',
+          boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+          transition: 'background-color 0.3s ease-in-out',
+        }}
+         onClick={saveNotesHandler}>
+            Save Note</button>
+            </div>
         <button
             style={{
                 display: "block",
