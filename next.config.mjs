f1d89600d83590899abd147ed0001ac8e9a35453
Abraham <abrahamnhier97@gmail.com>
2024-02-19 07:38:39 +0300

    typescript support

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    typescript: {
+        ignoreDevErrors: true,
+        ignoreBuildErrors: true,
+    }
+};
 
 export default nextConfig;
