2588d81d4e928c37b97fcd690b950e55dca274c2
Abraham <abrahamnhier97@gmail.com>
2024-02-20 12:35:04 +0300

    removed home icon from location page

--- app/location/page.tsx
+++ app/location/page.tsx
@@ -5,26 +5,11 @@ import { IoHomeOutline } from "react-icons/io5";
 import { useRouter } from 'next/navigation';
 import Locations from '../components/Location';
 
-
-
 export default function LocationPage() {
-    const router = useRouter()
-    const exitHandler = () => {
-        router.push('/');
-    }
+   
     return (
         <div>
-            
-            <IoHomeOutline
-            style={{
-              fontSize: "30px",
-             marginBottom: "10px",
-             cursor: "pointer",
-             backgroundColor: "#23d997"
-            }}
-            onClick={exitHandler}
-            />
-            <Locations/>
+        <Locations />
         </div>
     )
 }
